cc798a7a30ced8925e7a1123b0096ecbdc3ec91c
vuongly2411 <lyhuong241@gmail.com>
2021-05-18 19:39:40 +0700

    laisuat

---
+++ laisuat.js
@@ -0,0 +1,5 @@
+let a = +prompt('So tien vay: ');
+let r = +prompt('Lai suat: ');
+let n = +prompt('So nam vay tien: ');
+tienno = a * (1+ r*n);
+document.write('Tong so tien no ngan hang: ' + tienno);
---
+++ sumOfab.js
@@ -0,0 +1,3 @@
+let a = +prompt('Số thứ nhất a = ');
+let b = +prompt('Số thứ hai b = ');
+document.write('Tổng của hai số là: ' + (a+b));
